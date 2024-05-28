1218477c5fdbc245bf0ce1ea10eba734ca370d71
Pavel Yosifov <pyosifov99@gmail.com>
2024-05-29 00:54:29 +0300

    First_Programs

---
+++ Code_Pointers.py
@@ -0,0 +1,32 @@
+# variables
+x = 1
+x = x + 1
+print(x)
+
+# strings
+x = "abc"
+y = "de"
+z = x + y
+z
+
+z = z[1:3]
+z
+
+
+# functions
+def f(x):
+    y = x + 2
+    return y
+
+
+f(5)
+
+# if statements
+x = 2
+if x + 1 == 3:
+    print(x)
+
+# loops
+for i in range(1, 100):
+    if i % 2 == 0:
+        print(i)
