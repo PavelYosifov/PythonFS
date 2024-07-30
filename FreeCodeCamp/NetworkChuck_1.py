fc88a0401de75ad826897b6f0dbfc54b66769cf2
Pavel Yosifov <pyosifov99@gmail.com>
2024-07-30 19:18:51 +0300

    NetworkChuck 1st programs

---
+++ FreeCodeCamp/NetworkChuck_1.py
@@ -0,0 +1,18 @@
+print("Hello world!!!!!!!!!!")
+print('I am Iron Man')
+print("No, I am Tony Stark")
+print("No, I am Poppy")
+# We are practicing Python... IT'S AWESOME!!!
+
+# Multi line string
+print("""
+I am Iron Man
+No, I am Tony Stark
+No, I am Poppy
+      """)
+
+print("I am Iron Man. " + "No, I am Tony Stark. " + "No, I am Poppy")
+print("I am Iron Man.\n" + "No, I am Tony Stark.\n" +
+      "No, I am Poppy")  # \n - newline indicator character
+
+print("I am Poppy\n" * 100)  # it will print I am Poppy 100 times
